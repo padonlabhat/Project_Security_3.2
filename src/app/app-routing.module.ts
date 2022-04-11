d4133bacfa7c5926790b89562edf5ffe5be5acb2
BADSTARSS <boonsongprathip@gmail.com>
2022-04-11 12:58:38 +0700

    add RSAEN and RSADE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VigenereCipherComponent } from './codecopy/vigenere-cipher.component';
-
+import { RSADEComponent } from './rsa-de/rsa-de.component';
+import { RSAENComponent } from './rsa-en/rsa-en.component'; 
 const routes: Routes = [
   { path: 'codecopy', component: VigenereCipherComponent},
+  { path: 'rsaen', component: RSAENComponent},
+  { path: 'rsade', component: RSADEComponent}
 
 ];
 
