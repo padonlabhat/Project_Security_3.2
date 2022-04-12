883d5f9825e47a12cbf4761fea2071b475a23d15
BADSTARSS <boonsongprathip@gmail.com>
2022-04-12 15:31:11 +0700

    update rsa-ed

--- src/app/rsa-de/rsa-de.component.ts
+++ src/app/rsa-de/rsa-de.component.ts
@@ -1,66 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-rsa-de',
   templateUrl: './rsa-de.component.html',
   styleUrls: ['./rsa-de.component.css']
 })
 export class RSADEComponent implements OnInit {
-
+  N: number | undefined;
+  R: any;
+  E: number | undefined;
+  D: number | undefined;
   constructor() { }
 
   ngOnInit(): void {
   }
-  CT='ABCD'
+  cal(P: string, Q: string) {
+    const ra = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 
+      47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 
+      109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 
+      179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241];
 
- 
-  test1(CT :string){
-    this.CT = CT.toUpperCase();
-    console.log('Hello World!');
-    var str = this.CT
-    var arr = str.split("");
-    var Ptext = [];
-    var Pnumber = [];
-    console.log("arr :"+arr);
-    var key = 4
-    var i;
-    var Ctext;
-    for (i = 0; i < arr.length; i++) {
-    
-        console.log(arr[i]);
-        Ptext[i] = arr[i]
-        console.log("arr : "+arr);
-        console.log(arr);
-        if (Ptext[i]=="A"){Pnumber[i]=0+key}
-        else if (Ptext[i]=="B"){Pnumber[i]=1+key}
-        else if (Ptext[i]=="C"){Pnumber[i]=2+key}
-        else if (Ptext[i]=="D"){Pnumber[i]=3+key}
-        else if (Ptext[i]=="E"){Pnumber[i]=4+key}
-        else if (Ptext[i]=="F"){Pnumber[i]=5+key}
-        else if (Ptext[i]=="G"){Pnumber[i]=6+key}
-        else if (Ptext[i]=="H"){Pnumber[i]=7+key}
-        else if (Ptext[i]=="I"){Pnumber[i]=8+key}
-        else if (Ptext[i]=="J"){Pnumber[i]=9+key}
-        else if (Ptext[i]=="K"){Pnumber[i]=10+key}
-        else if (Ptext[i]=="L"){Pnumber[i]=11+key}
-        else if (Ptext[i]=="M"){Pnumber[i]=12+key}
-        else if (Ptext[i]=="N"){Pnumber[i]=13+key}
-        else if (Ptext[i]=="O"){Pnumber[i]=14+key}
-        else if (Ptext[i]=="P"){Pnumber[i]=15+key}
-        else if (Ptext[i]=="Q"){Pnumber[i]=16+key}
-        else if (Ptext[i]=="R"){Pnumber[i]=17+key}
-        else if (Ptext[i]=="S"){Pnumber[i]=18+key}
-        else if (Ptext[i]=="T"){Pnumber[i]=19+key}
-        else if (Ptext[i]=="U"){Pnumber[i]=20+key}
-        else if (Ptext[i]=="V"){Pnumber[i]=21+key}
-        else if (Ptext[i]=="W"){Pnumber[i]=22+key}
-        else if (Ptext[i]=="X"){Pnumber[i]=23+key}
-        else if (Ptext[i]=="Y"){Pnumber[i]=24+key}
-        else if (Ptext[i]=="Z"){Pnumber[i]=25+key}
-        else Ptext[i]="ERROR";
-        Pnumber[i]=Pnumber[i]%26
+
+    console.log("hello world");
+    this.N = parseInt(P) * parseInt(Q);
+    console.log("N : " + this.N);
+
+    this.R = (parseInt(P) - 1) * (parseInt(Q) - 1);
+    console.log("R : " + this.R);
+
+    var b = this.getRandomDifferent(ra);
+    let A = this.check(b);
+    this.E = A;
+    console.log("E : " + this.E);
+
+    this.D = 1/(A)%parseInt(this.R);
+    console.log("D : " + this.D);
+
+  }
+  getRandomDifferent(arr: string | any[], last = undefined) {
+    if (arr.length === 0) {
+      return null;
+    } else if (arr.length === 1) {
+      return arr[0];
+    } else {
+      let num = 0;
+      do {
+        num = Math.floor(Math.random() * arr.length);
+      } while (arr[num] === last);
+      return arr[num];
     }
-    console.log(Ptext);
-    console.log(Pnumber);
+  }
+  check(val: number) {
+    if (val <= this.R) {
+      console.log("val :" + val);
+      return val
+    } else {
+      return 0;
+    }
   }
 }
